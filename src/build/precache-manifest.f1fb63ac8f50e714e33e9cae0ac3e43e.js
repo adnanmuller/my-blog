d7e29aa4f5086069dd1d4dccdfb61bb800edb48f
adnan <adnan172@orange.fr>
2020-09-12 00:32:08 +0200

    First commit

---
+++ src/build/precache-manifest.f1fb63ac8f50e714e33e9cae0ac3e43e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f5d2f5272a1069b3d215411542a9e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d9bffc22cb2963dc0e2",
+    "url": "/static/css/main.3513c0ce.chunk.css"
+  },
+  {
+    "revision": "c111e6ab5ad091c19bc7",
+    "url": "/static/js/2.b1ace6ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b1ace6ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9bffc22cb2963dc0e2",
+    "url": "/static/js/main.118c2472.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
